593bcf4f20cee2d6c21c66917149f927c30fb2ab
Gerardo Camilo <gerardo.camilo@gmail.com>
2015-07-07 22:04:21 -0400

    First commit, managing audio with Swift.

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -6,15 +6,48 @@
 //  Copyright (c) 2015 ___GRCS___. All rights reserved.
 //
 
+import AVFoundation
+
 import UIKit
+
+var globalVariable:NSString = ""
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerValue: UISlider!
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func play(sender: AnyObject) {
+        var audioPath = NSBundle.mainBundle().pathForResource("chopin-9-1-hammer", ofType: "mp3")
+        
+        var error:NSError? = nil
+        
+        player = AVAudioPlayer(contentsOfURL: NSURL(string: audioPath!), error: &error)
+        
+        if error == nil {
+            player.play()
+        } else {
+            println(error)
+        }
+        
+    }
 
+    @IBAction func pause(sender: AnyObject) {
+        if player.playing {
+            player.pause()
+        } else {
+            player.play()
+        }
+    }
+    @IBAction func sliderChanged(sender: AnyObject) {
+        
+        player.volume = playerValue.value
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
